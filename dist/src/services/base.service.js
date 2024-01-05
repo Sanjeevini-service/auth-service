a767c791916c331156eedf115422f565c8045fbb
vinayak-naik <vinayaknaik2021@gmail.com>
2024-01-05 22:37:27 +0530

    dist deleted

--- dist/src/services/base.service.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class BaseService {
-    constructor(modelI) {
-        this.model = modelI.model;
-    }
-}
-exports.default = BaseService;
