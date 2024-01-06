7244a146f1319622447a08dc910df858cc192e7e
vinayak-naik <vinayaknaik2021@gmail.com>
2024-01-06 17:33:24 +0530

    minor 2

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -10,22 +10,22 @@ async function sendEmail(payload: SendMailOptions): Promise<boolean> {
     console.log("senderEmail===========", senderEmail);
     console.log("senderEmailPassword===========", senderEmailPassword);
 
-    // const transporter = nodemailer.createTransport(
-    //   `smtp://${senderEmail}:${senderEmailPassword}@smtp-mail.outlook.com`
-    // );
+    const transporter = nodemailer.createTransport(
+      `smtp://${senderEmail}:${senderEmailPassword}@smtp-mail.outlook.com`
+    );
 
-    const transporter = nodemailer.createTransport({
-      host: "smtp-mail.outlook.com", // hostname
-      secureConnection: false, // TLS requires secureConnection to be false
-      port: 587, // port for secure SMTP
-      auth: {
-        user: senderEmail,
-        pass: senderEmailPassword,
-      },
-      tls: {
-        ciphers: "SSLv3",
-      },
-    });
+    // const transporter = nodemailer.createTransport({
+    //   host: "smtp-mail.outlook.com", // hostname
+    //   secureConnection: false, // TLS requires secureConnection to be false
+    //   port: 587, // port for secure SMTP
+    //   auth: {
+    //     user: senderEmail,
+    //     pass: senderEmailPassword,
+    //   },
+    //   tls: {
+    //     ciphers: "SSLv3",
+    //   },
+    // });
 
     await transporter.sendMail(payload);
     log.info("Email sent successfully");
